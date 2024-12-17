e8d06257e77a070d4fedc003dd2da64db724cbce
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2024-12-17 18:46:16 +0300

    Update Header.jsx

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
      <h1><span>I'm Kiana Ramzanzadeh,</span> Frontend developer based in Turkiye</h1>
      <p>I am an Iranian frontend developer from Turkiye, trying to start my career in Front-End developer field</p>
      <div className="header-action">
-        <div className="header-cv"><a href="../assets/CV.pdf" download="KianaRamzanzadeh_CV.pdf">  &#10515; My resume</a></div>
+        <div className="header-cv"><a href="" download="KianaRamzanzadeh_CV.pdf">  &#10515; My resume</a></div>
      </div>
     </div>
     
