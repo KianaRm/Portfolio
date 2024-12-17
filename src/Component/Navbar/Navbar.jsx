7d0a22c8c62e09bad7e73c4e59f75e725115d71e
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2024-12-17 14:57:26 +0300

    Navbar.jsx

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -22,7 +22,10 @@ const Navbar = () => {
 
   return (
     <div className="navbar"> 
-      <img src={logo} alt="" />
+       <div className="nav-logo">
+        <h1>KianWeb</h1>
+        <img src={theme_pattern} alt=""/>
+      </div>
       <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open"/>
       <ul ref={menuRef} className="nav-menu">
         <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
